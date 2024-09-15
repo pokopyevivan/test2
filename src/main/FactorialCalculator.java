4a440692afb04d127e53f987fe391896cbcfe511
ivan <ivan-rtk@mail.ru>
2024-09-15 09:23:44 +0300

    dz2

---
+++ src/main/FactorialCalculator.java
@@ -0,0 +1,14 @@
+package main;
+
+public class FactorialCalculator {
+    public static long factorial(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Число должно быть неотрицательным");
+        }
+        long result = 1;
+        for (int i = 1; i <= number; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
